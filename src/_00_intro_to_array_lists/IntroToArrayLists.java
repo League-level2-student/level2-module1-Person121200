37e73e51702ccea86e58c464bff350a0624e52b5
league <maddoxseul@sbsd.net>
2024-05-12 11:27:36 -0700

    Working on GuestBook

--- src/_00_intro_to_array_lists/IntroToArrayLists.java
+++ src/_00_intro_to_array_lists/IntroToArrayLists.java
@@ -1,20 +1,44 @@
 package _00_intro_to_array_lists;
 
+import java.util.ArrayList;
+
 public class IntroToArrayLists {
 	public static void main(String[] args) {
-		//1. Create an array list of Strings
-		//   Don't forget to import the ArrayList (java.util) class
+		// 1. Create an array list of Strings
+		// Don't forget to import the ArrayList (java.util) class
+		ArrayList<String> strings = new ArrayList<String>();
+		// 2. Add five Strings to your list
+		strings.add("sadness");
+		strings.add("excitement");
+		strings.add("anger");
+		strings.add("happiness");
+		strings.add("anxiety");
+		// 3. Print all the Strings using a standard for-loop
+		for (int i = 0; i < strings.size(); i++) {
+			System.out.println(strings.get(i));
+		}
+//		//4. Print all the Strings using a for-each loop
+		for (String s : strings) {
+			System.out.println(s);
+		}
+		// 5. Print only the even numbered elements in the list.
+		for (int j = 0; j < strings.size(); j++) {
+			if (j % 2 != 0) {
+				System.out.println(strings.get(j));
+			}
+		}
+		// 6. Print all the Strings in reverse order.
+		for(int t = strings.size()-1; t>=0; t--) {
+			System.out.println(strings.get(t));
+		}
+		
+		System.out.println();
+		for(int l = 0; l< strings.size(); l++) {
+			if(strings.get(l).contains("e")){
+				System.out.println(strings.get(l));
+			}
+		}
 
-		//2. Add five Strings to your list
-		
-		//3. Print all the Strings using a standard for-loop
-		
-		//4. Print all the Strings using a for-each loop
-		
-		//5. Print only the even numbered elements in the list.
-		
-		//6. Print all the Strings in reverse order.
-		
-		//7. Print only the Strings that have the letter 'e' in them.
+		// 7. Print only the Strings that have the letter 'e' in them.
 	}
 }
--- src/_01_array_list_visualizer/VisualArrayLists.java
+++ src/_01_array_list_visualizer/VisualArrayLists.java
@@ -11,31 +11,42 @@ import _01_array_list_visualizer.data.ArrayList;
 public class VisualArrayLists implements ActionListener {
 	Timer timer;
 	JFrame frame = new JFrame();
-	
+
 	ArrayList<String> list = new ArrayList<String>();
 
 	public void addThingsToList() {
 		// 1. Add a bunch of items to our list (4 or 5 should be enough!)
-		
+		list.add("swimming");
+		list.add("basketball");
+		list.add("golf");
+		list.add("baseball");
+		list.add("ping pong");
 		// 2. Set one item in our list to be something different
-		
+		list.set(4, "tennis");
+
 		// 3. Remove something from our list
-		
+		list.remove(2);
 		// 4. Print the size of our list
-		
+
+		System.out.println(list.size());
 		// 5. Check if our list contains a specific thing of your choice
-		//	  and print whether it is in the list or not.
-		
+		// and print whether it is in the list or not.
+		for (int i = 0; i < list.size(); i++) {
+			if (list.get(i).contains("e")) {
+				System.out.println(list.get(i));
+			}
+		}
 		// 6. Get a specific item from our list and print it out
-		
+		System.out.println(list.get(3));
 		// 7. Clear our list of all items
-		
+		list.clear();
 		// 8. Print the size of our list again
+		System.out.println(list.size());
 	}
-	
-	
+
 	/*
-	 * ------------DONT CHANGE ANYTHING BELOW THIS LINE-----------------------------------------
+	 * ------------DONT CHANGE ANYTHING BELOW THIS
+	 * LINE-----------------------------------------
 	 */
 	public static void main(String[] args) {
 		VisualArrayLists vcc = new VisualArrayLists();
