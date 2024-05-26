de188f5811993e40890de8db6391c0e35db30ed7
league <league@localhost>
2024-05-26 10:28:12 -0700

    FINISHED MODULE

--- src/_02_array_list_guestbook/GuestBook.java
+++ src/_02_array_list_guestbook/GuestBook.java
@@ -1,6 +1,41 @@
 package _02_array_list_guestbook;
 
-public class GuestBook {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class GuestBook implements ActionListener{
+	JButton addName = new JButton("Add Name");
+	JButton viewName = new JButton("View name");
+	JPanel panel = new JPanel();
+	JFrame frame = new JFrame();
+	ArrayList<String> names = new ArrayList<String>();
+	
+	public void show() {
+		addName.addActionListener(this);
+		viewName.addActionListener(this);
+		panel.add(addName);
+		panel.add(viewName);
+		frame.add(panel);
+		frame.setVisible(true);
+		frame.pack();
+		
+	}
+	
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource() == addName) {
+			String name = JOptionPane.showInputDialog("Please add a name: ");
+			names.add(name);
+		}
+		if(e.getSource() == viewName) {
+			JOptionPane.showMessageDialog(null, names);
+		}
+	}
 	// Create a GUI with two buttons. One button reads "Add Name" and the other button reads "View Names". 
 	// When the add name button is clicked, display an input dialog that asks the user to enter a name. Add
 	// that name to an ArrayList. When the "View Names" button is clicked, display a message dialog that displays
@@ -9,4 +44,5 @@ public class GuestBook {
 	// Guest #2: Sandy Summers
 	// Guest #3: Greg Ganders
 	// Guest #4: Donny Doners
+
 }
